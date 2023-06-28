8db46d3ac35d6830eaf764e5f5f6f2c07b499a30
Jon <jroach@fusionsiteservices.com>
2023-06-28 08:23:03 -0500

    more ch 2 exercises

---
+++ Ch2P3/Ch2P3.fs
@@ -0,0 +1,16 @@
+﻿(*Declare the F# function isIthChar: string*int*char -> bool
+where the value of isIthChar(str, i, ch) is true if and only
+if ch is the i'th character in the string str (numbering
+starting at 0)*)
+
+let isIthChar (str :string) (i :int) (ch :char) = 
+    if i > 0 && i < String.length str then
+        str.[i] = ch
+    else
+        false
+
+printf "%b\n" (isIthChar "Hello" 0 'H')
+printf "%b\n" (isIthChar "Hello" 2 'o')
+printf "%b\n" (isIthChar "Hello" 4 'o')
+
+
---
+++ Ch2P4/Ch2P4.fs
@@ -0,0 +1,16 @@
+﻿(*Declare the F# function occFromIth: string * int * char -> int 
+where occFromIth(str, i, ch) = the number of occurrences of character
+ch in positions j in the string str with j >= i*)
+
+let rec occFromIth (str :string) (i :int) (ch :char) =
+    match i with 
+    | _ when i >= String.length str -> 0
+    | _ when str.[i] = ch -> 1 + occFromIth str (i + 1) ch
+    | _ -> occFromIth str (i + 1) ch
+    
+let str = "Hello, world!"
+let charToCount = 'o'
+let startIndex = 3
+let occurrences = occFromIth str startIndex charToCount
+
+printfn "Number of occurrences of '%c' from index %d onwards: %d" charToCount startIndex occurrences
